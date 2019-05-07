e9741eb625b281d51659019529ecd9828b6a1c92
Mohamed <mohamed.khammeri@esprit.tn>
2019-05-07 01:19:57 +0100

    Fix Config

--- config.php
+++ config.php
@@ -28,7 +28,7 @@ class config
     {
         if (!isset(self::$instance)) {
             try {
-                self::$instance = new PDO('mysql:host=localhost;dbname=obladi', 'root', 'root');
+                self::$instance = new PDO('mysql:host=localhost;dbname=obladi', 'root', '');
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (Exception $e) {
                 die('Erreur: ' . $e->getMessage());
